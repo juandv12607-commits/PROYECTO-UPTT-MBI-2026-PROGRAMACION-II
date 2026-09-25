cc9840a2ca6fb1de72d8286fad351f09acc8da19
juandv12607-commits <juandv12607@gmail.com>
2026-09-24 23:50:14 -0400

    Change server port and update listen method
    Changed server port from 3000 to 10000 and updated listen method to bind to all interfaces.

--- My Proyect/Server.js
+++ My Proyect/Server.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
 const cors = require('cors'); // Puedes quitarlo si no vas a usar otros puertos
 const app = express();
-const port = 3000;
+const port = 10000;
 
 // 1. Deshabilitar o simplificar CORS ya que todo corre en localhost:3000
 app.use(cors()); 
@@ -21,6 +21,6 @@ app.use(express.static(path.join(__dirname, 'Views')));
 // 4. Rutas de la API
 app.use('/api', router);
 
-app.listen(port, () => {
+app.listen(port,'0.0.0.0', () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });

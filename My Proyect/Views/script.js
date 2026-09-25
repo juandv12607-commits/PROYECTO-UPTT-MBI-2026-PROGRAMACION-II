3c7e6b6152d14049061e11191e5a3d57201cb499
juandv12607-commits <juandv12607@gmail.com>
2026-09-25 14:05:45 -0400

    Update login API URL to production endpoint

--- My Proyect/Views/script.js
+++ My Proyect/Views/script.js
@@ -158,7 +158,7 @@ export async function newprompt(option,p,column,table,id){
             }else if(values.fun==='login'){
               // --- LOGIN ---
   async function login(name, password) {
-    const res = await fetch('http://localhost:3000/api/login', {
+    const res = await fetch('https://proyecto-uptt-mbi-2026-programacion-ii.onrender.com/api/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       credentials: 'include',   // ← ¡envía y recibe cookies!
